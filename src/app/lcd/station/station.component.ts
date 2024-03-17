417c553d680e5860102c84e44f53f69660789125
wheeljs <ttvip88888888@foxmail.com>
2024-03-17 20:37:39 +0800

    refactor(lcd/station): edit mode ux
    Add wrap element for <lcd-station> for binding (clickOutside).
    Add ng-outside-click2.

--- src/app/lcd/station/station.component.ts
+++ src/app/lcd/station/station.component.ts
@@ -1,4 +1,5 @@
 import { Component, ViewEncapsulation, Input } from '@angular/core';
+import { faGear } from '@fortawesome/free-solid-svg-icons';
 import { StationStatus, type RunningLine, type RunningLineStation } from '../types';
 import { ContainerConfigService } from '../container-config.service';
 
@@ -6,21 +7,18 @@ import { ContainerConfigService } from '../container-config.service';
   selector: 'lcd-station',
   templateUrl: './station.component.html',
   styleUrl: './station.component.scss',
-  host: {
-    class: 'lcd-station',
-    '[class.lcd-station--transferrable]': `transferrable`,
-    '[class.lcd-station--disabled]': `station.disabled`,
-    '[class.lcd-station--arriving-soon]': `station.status === StationStatus.ArrivingSoon`,
-    '[class.lcd-station--past]': `station.status === StationStatus.Past || station.status === StationStatus.NotInService`,
-  },
   encapsulation: ViewEncapsulation.None,
 })
 export class StationComponent {
+  settingIcon = faGear;
+
   StationStatus = StationStatus;
 
   @Input() line?: RunningLine;
 
   @Input() station!: RunningLineStation;
+
+  showEditingBar = false;
 
   get transferrable(): boolean {
     return this.station?.transfers?.some((x) => !x.disabled) ?? false;
@@ -31,4 +29,12 @@ export class StationComponent {
   }
 
   constructor(private containerConfigService: ContainerConfigService) {}
+
+  toggleEditingBar() {
+    this.showEditingBar = !this.showEditingBar;
+  }
+
+  clickOutside() {
+    this.showEditingBar = false;
+  }
 }
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { NgClickOutsideDirective } from 'ng-click-outside2';
 import { StripNewLinePipe } from './strip-new-line.pipe';
 
-const ExportsAsIs = [CommonModule, FontAwesomeModule];
+const ExportsAsIs = [CommonModule, FontAwesomeModule, NgClickOutsideDirective];
 
 @NgModule({
   declarations: [StripNewLinePipe],
