bc5926892c1a748768bb1fd3c851922be4d8d898
wheeljs <ttvip88888888@foxmail.com>
2024-04-20 18:39:28 +0800

    refactor(dashboard): change <mat-spinner> to <nz-spin>

--- src/app/dashboard/dashboard-ui.module.ts
+++ src/app/dashboard/dashboard-ui.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
-import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
 import { MatSelectModule } from '@angular/material/select';
 import { MatSidenavModule } from '@angular/material/sidenav';
 
@@ -10,7 +9,6 @@ const ExportsAsIs = [
   MatButtonModule,
   MatCardModule,
   MatFormFieldModule,
-  MatProgressSpinnerModule,
   MatSelectModule,
   MatSidenavModule,
 ];
