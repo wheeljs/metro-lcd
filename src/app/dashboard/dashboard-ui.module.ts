f01ce257d039115ae6c55d0341b738c7a7c2cfa6
wheeljs <ttvip88888888@foxmail.com>
2024-05-03 15:50:13 +0800

    feat(dashboard/monthly-data): add icon for main data cards

--- src/app/dashboard/dashboard-ui.module.ts
+++ src/app/dashboard/dashboard-ui.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { catchError, forkJoin, map, of } from 'rxjs';
 import { NzButtonModule } from 'ng-zorro-antd/button';
 import { NzCardModule } from 'ng-zorro-antd/card';
 import { NzDividerModule } from 'ng-zorro-antd/divider';
 import { NzFlexModule } from 'ng-zorro-antd/flex';
 import { NzFormModule } from 'ng-zorro-antd/form';
 import { NzGridModule } from 'ng-zorro-antd/grid';
-import { NzIconModule } from 'ng-zorro-antd/icon';
+import { NzIconModule, NzIconService } from 'ng-zorro-antd/icon';
 import { NzPopoverModule } from 'ng-zorro-antd/popover';
 import { NzSelectModule } from 'ng-zorro-antd/select';
 import { NzSpaceModule } from 'ng-zorro-antd/space';
@@ -15,7 +17,12 @@ import { NzStatisticModule } from 'ng-zorro-antd/statistic';
 import { NgxEchartsDirective, provideEchartsCore } from 'ngx-echarts';
 
 import type { IconDefinition } from '@ant-design/icons-angular';
-import { ArrowLeftOutline, InfoCircleOutline, LeftOutline } from '@ant-design/icons-angular/icons';
+import {
+  ArrowLeftOutline,
+  InfoCircleOutline,
+  LeftOutline,
+  PlusSquareFill,
+} from '@ant-design/icons-angular/icons';
 
 import echarts from '../shared/echarts/custom-echarts';
 
@@ -23,6 +30,7 @@ const icons: IconDefinition[] = [
   ArrowLeftOutline,
   InfoCircleOutline,
   LeftOutline,
+  PlusSquareFill,
 ];
 
 const ExportsAsIs = [
@@ -41,6 +49,11 @@ const ExportsAsIs = [
   NgxEchartsDirective,
 ];
 
+interface AdditionalIconDefinition {
+  icon: string;
+  svg: string;
+};
+
 @NgModule({
   imports: [
     ...ExportsAsIs,
@@ -57,4 +70,26 @@ const ExportsAsIs = [
     }),
   ],
 })
-export class DashboardUiModule {}
+export class DashboardUiModule {
+  constructor(httpClient: HttpClient, nzIconService: NzIconService) {
+    forkJoin(
+      ['line', 'operation-length', 'segment', 'train'].map((icon) =>
+        httpClient.get(`/assets/imgs/${icon}.svg`, { responseType: 'text' }).pipe(
+          map((iconSvg) => ({
+            icon,
+            svg: iconSvg,
+          } as AdditionalIconDefinition)),
+          catchError(() => of(null))
+        )
+      )
+    ).pipe(
+      map((icons) => icons.filter((x) => x != null) as AdditionalIconDefinition[]),
+    ).subscribe({
+      next: (icons) => {
+        icons.forEach(({ icon, svg }) => {
+          nzIconService.addIconLiteral(`dashboard:${icon}`, svg);
+        });
+      }
+    });
+  }
+}
