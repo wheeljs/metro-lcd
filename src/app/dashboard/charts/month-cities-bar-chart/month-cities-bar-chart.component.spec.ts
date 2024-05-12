e3ee7f18acb455cff49cf6c408362085b76ec2b1
wheeljs <ttvip88888888@foxmail.com>
2024-05-12 20:39:04 +0800

    test(dashboard/chart/month-cities): unit test

--- src/app/dashboard/charts/month-cities-bar-chart/month-cities-bar-chart.component.spec.ts
+++ src/app/dashboard/charts/month-cities-bar-chart/month-cities-bar-chart.component.spec.ts
@@ -1,5 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { By } from '@angular/platform-browser';
+import { NgxEchartsDirective, provideEchartsCore } from 'ngx-echarts';
+import type { DatasetComponentOption, ECElementEvent } from 'echarts';
+import echarts from '../../../shared/echarts/custom-echarts';
 import { MonthCitiesBarChartComponent } from './month-cities-bar-chart.component';
 
 describe('MonthCitiesBarChartComponent', () => {
@@ -8,10 +11,14 @@ describe('MonthCitiesBarChartComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [MonthCitiesBarChartComponent]
+      imports: [NgxEchartsDirective],
+      declarations: [MonthCitiesBarChartComponent],
+      providers: [
+        provideEchartsCore({ echarts }),
+      ],
     })
     .compileComponents();
-    
+
     fixture = TestBed.createComponent(MonthCitiesBarChartComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +26,48 @@ describe('MonthCitiesBarChartComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should generate correct options when set cities', () => {
+    component.cities = [
+      {
+        id: 'shanghai',
+        number: 1,
+        city: '上海',
+        lines: 22,
+        operationLength: 864.8,
+        passengerCapacity: 327607000,
+        inStationCapacity: 181983000,
+        passengerStrong: 1.26,
+      }
+    ];
+
+    const citiesDataset = (component.merge.dataset as DatasetComponentOption[])[0];
+    const citiesSource = citiesDataset.source as DatasetComponentOption['source'];
+    expect((citiesSource as any[])[0]).toEqual({
+      id: 'shanghai',
+      number: 1,
+      city: '上海',
+      lines: 22,
+      operationLength: 864.8,
+      passengerCapacity: 32760.7,
+      inStationCapacity: 18198.3,
+      passengerStrong: 1.26,
+    });
+  });
+
+  it('should show back button when drilldowned', async () => {
+    await fixture.whenStable();
+    component.onChartClick({
+      value: {
+        id: 'shanghai',
+        drilldownable: true,
+      },
+    } as unknown as ECElementEvent);
+    fixture.detectChanges();
+    await fixture.whenStable();
+
+    const backButton = fixture.debugElement.query(By.css('.toolbars button')).nativeElement as HTMLElement;
+    expect(backButton).toBeTruthy();
   });
 });
