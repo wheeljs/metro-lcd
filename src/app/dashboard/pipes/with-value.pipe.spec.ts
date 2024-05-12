aa91184411169c2724de648b796656c5d8a6d411
wheeljs <ttvip88888888@foxmail.com>
2024-05-12 18:48:21 +0800

    test(dashboard/pipes/with-value): unit test

--- src/app/dashboard/pipes/with-value.pipe.spec.ts
+++ src/app/dashboard/pipes/with-value.pipe.spec.ts
@@ -1,8 +1,27 @@
 import { WithValuePipe } from './with-value.pipe';
 
 describe('WithValuePipe', () => {
+  let pipe: WithValuePipe;
+
+  beforeEach(() => {
+    pipe = new WithValuePipe();
+  });
+
   it('create an instance', () => {
-    const pipe = new WithValuePipe();
     expect(pipe).toBeTruthy();
+  });
+
+  it('should filter out values with corresponding falsy vars', () => {
+    const values = ['value1', 'value2', 'value3'];
+    const vars = [0, 'truthy', null];
+    const result = pipe.transform(values, vars);
+    expect(result).toEqual(['value2']);
+  });
+
+  it('should handle array vars', () => {
+    const values = ['value1', 'value2', 'value3'];
+    const vars = [[0, '', null], ['truthy', 0], [null, undefined]];
+    const result = pipe.transform(values, vars);
+    expect(result).toEqual(['value2']);
   });
 });
